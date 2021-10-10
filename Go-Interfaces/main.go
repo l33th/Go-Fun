5c062f6a50ffdab9501e0c3374f931d6e68222fd
l33th <l33th@protonmail.ch>
2021-10-10 07:10:03 +0200

    Interfaces

--- Go-Interfaces/main.go
+++ Go-Interfaces/main.go
@@ -106,6 +106,31 @@ type Article struct {
 func (a Article) String() string {
     return fmt.Sprintf("The %q article was written by %s.", a.Title, a.Author)
 }
+
+type tank interface {
+  
+    // Methods
+    Tarea() float64
+    Volume() float64
+}
+  
+type myvalue struct {
+    radius float64
+    height float64
+}
+  
+// Implementing methods of
+// the tank interface
+func (m myvalue) Tarea() float64 {
+  
+    return 2*m.radius*m.height +
+        2*3.14*m.radius*m.radius
+}
+  
+func (m myvalue) Volume() float64 {
+  
+    return 3.14 * m.radius * m.radius * m.height
+}
  
 func main() {
     var i interface{}
@@ -154,4 +179,9 @@ func main() {
         Author: "WM",
     }
     fmt.Println(art.String())
+
+    var t tank
+    t = myvalue{10, 14}
+    fmt.Println("Area of tank :", t.Tarea())
+    fmt.Println("Volume of tank:", t.Volume())
 }
