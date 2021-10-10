6b67e86be54a187d6088aece2488c9f953535778
l33th <l33th@protonmail.ch>
2021-10-10 06:50:18 +0200

    Interfaces

--- Go-Interfaces/main.go
+++ Go-Interfaces/main.go
@@ -3,8 +3,28 @@ package main
 import (
     "fmt"
 )
+
+type Person interface{
+    greet() string
+}
+ 
+type Human struct{
+    Name string
+}
+ 
+func (h *Human)greet() string {
+    return "Hi, I am " + h.Name 
+}
+ 
+func isAPerson(h Person) {
+    fmt.Println(h.greet())
+}
  
 func main() {
-    var a interface{}
-    fmt.Println(a)
+    var i interface{}
+    var h = Human{"Glarbouered"}
+    fmt.Println(i) 
+    fmt.Println(h.greet())
+
+    isAPerson(&h)
 }
