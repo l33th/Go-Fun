e8b2f24f7e1c71044ce021f0e3339e2445dbb270
l33th <l33th@protonmail.ch>
2021-10-10 18:37:54 +0200

    Channels in Go - Calculate

--- Go-Channels/main.go
+++ Go-Channels/main.go
@@ -15,6 +15,27 @@ func hello(done chan bool) {
     done <- true
 }
 
+func calcSquares(number int, squareop chan int) {  
+    sum := 0
+    for number != 0 {
+        digit := number % 10
+        sum += digit * digit
+        number /= 10
+    }
+    squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {  
+    sum := 0 
+    for number != 0 {
+        digit := number % 10
+        sum += digit * digit * digit
+        number /= 10
+    }
+    cubeop <- sum
+} 
+
+
 func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
@@ -29,4 +50,12 @@ func main() {
     go hello(done)
     <-done
     fmt.Println("main function")
+
+    number := 589
+    sqrch := make(chan int)
+    cubech := make(chan int)
+    go calcSquares(number, sqrch)
+    go calcCubes(number, cubech)
+    squares, cubes := <-sqrch, <-cubech
+    fmt.Println("Final output", squares + cubes)
 }
