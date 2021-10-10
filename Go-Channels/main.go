c863e7835cd83a9f29a3a7f858fe0e985d1c6c2b
l33th <l33th@protonmail.ch>
2021-10-10 18:46:30 +0200

    Channels For Range

--- Go-Channels/main.go
+++ Go-Channels/main.go
@@ -75,11 +75,7 @@ func main() {
 
     ch := make(chan int)
     go producer(ch)
-    for {
-        v, ok := <-ch
-        if ok == false {
-            break
-        }
-        fmt.Println("Received ", v, ok)
+    for v := range ch {
+        fmt.Println("Received ",v)
     }
 }
