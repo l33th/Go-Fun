c6e70328dd24d45533074d7f602f0079a58200c9
l33th <l33th@protonmail.ch>
2021-10-10 07:03:36 +0200

    Interfaces

--- Go-Interfaces/main.go
+++ Go-Interfaces/main.go
@@ -2,6 +2,7 @@ package main
  
 import (
     "fmt"
+    "math"
 )
 
 type Person interface{
@@ -66,6 +67,36 @@ func isEqual(i interface{}, j interface{}) {
 func f(i interface{}) {
     fmt.Printf("%T\n", i)
 }
+
+/* define an interface */
+type Shape interface {
+    area() float64
+}
+
+/* define a circle */
+type Circle struct {
+    x,y,radius float64
+}
+
+/* define a rectangle */
+type Rectangle struct {
+    width, height float64
+}
+
+/* define a method for circle (implementation of Shape.area())*/
+func(circle Circle) area() float64 {
+    return math.Pi * circle.radius * circle.radius
+}
+
+/* define a method for rectangle (implementation of Shape.area())*/
+func(rect Rectangle) area() float64 {
+    return rect.width * rect.height
+}
+
+/* define a method for shape */
+func getArea(shape Shape) float64 {
+    return shape.area()
+}
  
 func main() {
     var i interface{}
@@ -102,4 +133,10 @@ func main() {
      
     f(ac)   // string
     f(cc)   // int
+
+circle := Circle{x:0,y:0,radius:5}
+   rectangle := Rectangle {width:10, height:5}
+   
+   fmt.Printf("Circle area: %f\n",getArea(circle))
+   fmt.Printf("Rectangle area: %f\n",getArea(rectangle))
 }
