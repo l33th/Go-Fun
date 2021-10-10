93ae5d7b40bc0601ad9aa025ebd9e63e17378647
l33th <l33th@protonmail.ch>
2021-10-10 06:52:11 +0200

    More Interfaces

--- Go-Interfaces/main.go
+++ Go-Interfaces/main.go
@@ -19,12 +19,38 @@ func (h *Human)greet() string {
 func isAPerson(h Person) {
     fmt.Println(h.greet())
 }
+
+type Flyer interface{
+    fly() string
+}
+ 
+type Walker interface{
+    walk() string
+}
+ 
+type Bird struct{
+    Name string
+}
+ 
+func (b *Bird)fly() string{
+    return "Flying..."
+}
+ 
+func (b *Bird)walk() string{
+    return "Walking..."
+}
  
 func main() {
     var i interface{}
     var h = Human{"Glarbouered"}
+
     fmt.Println(i) 
     fmt.Println(h.greet())
 
     isAPerson(&h)
+
+    var b = Bird{"Chirper"}
+     
+    fmt.Println(b.fly())
+    fmt.Println(b.walk())
 }
