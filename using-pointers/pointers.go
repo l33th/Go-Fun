5a4d61e075a8ff0ff907f13b737dd589bc798a37
l33th <l33th@protonmail.ch>
2021-10-21 07:32:12 +0200

    yValue before

--- using-pointers/pointers.go
+++ using-pointers/pointers.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 	xValue := 0
-	fmt.Println("xValue Pointer before: ", &xValue)
+	fmt.Println("xValue Address before: ", &xValue)
 	fmt.Println("xValue Variable Before: ", xValue)
+
+	foo(&xValue)
+}
+
+func foo(yValue *int) {
+	fmt.Println("yValue Variable before: ", yValue)
+	fmt.Println("yValue pointer before: ", *yValue)
 }
